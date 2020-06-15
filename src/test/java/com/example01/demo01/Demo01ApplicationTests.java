7d2bf2bab179f5c86cbfc29c2de8fb6814a683d9
biaoge123456 <1017545773@qq.com>
2020-06-15 14:32:41 +0800

    组员A

--- src/test/java/com/example01/demo01/Demo01ApplicationTests.java
+++ src/test/java/com/example01/demo01/Demo01ApplicationTests.java
@@ -3,11 +3,17 @@ package com.example01.demo01;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 @SpringBootTest
 class Demo01ApplicationTests {
 
     @Test
     void contextLoads() {
+        ArrayList<String> list=new ArrayList<>();
+        Collections.addAll(list,"min","max","sum","avg");
+        list.stream().forEach(s-> System.out.println(s));
     }
 
 }
